8d860ece462ab59bbf38df1fc009732c4d1e0522
Andrew Rabert <andrewr@aweber.com>
2025-08-12 16:57:21 -0400

    git-auto-update 2025-08-12T20:57:21+00:00

--- nvim/lua/plugins/spec1.lua
+++ nvim/lua/plugins/spec1.lua
@@ -63,4 +63,26 @@ return {
     end,
   },
   {"dense-analysis/ale"},
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>-",
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+    },
+    opts = {
+      open_for_directories = false,
+      keymaps = {
+        show_help = '<f1>',
+      },
+    },
+  },
 }
