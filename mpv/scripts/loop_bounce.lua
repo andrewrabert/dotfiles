092a687bce248f95a71e7bda5a7355bbb8642d04
Andrew Rabert <ar@nullsum.net>
2026-03-14 15:06:23 -0400

    git-auto-update 2026-03-14T19:06:23+00:00

--- mpv/scripts/loop_bounce.lua
+++ mpv/scripts/loop_bounce.lua
@@ -1,15 +1,14 @@
 local loop_a
 local loop_b
 local tmp_a
-local dir
 
 local function bouncing_loop(_, time_pos)
 	if time_pos then
-		dir = mp.get_property("play-dir")
+		local dir = mp.get_property("play-direction")
 		if dir == "forward" and (time_pos >= loop_b) then
-			mp.commandv("cycle", "play-dir")
+			mp.commandv("cycle", "play-direction")
 		elseif dir == "backward" and (time_pos <= loop_a) then
-			mp.commandv("cycle", "play-dir")
+			mp.commandv("cycle", "play-direction")
 		end
 	end
 end
@@ -30,11 +29,14 @@ mp.add_key_binding(";", "bouncing-loop", function()
 			return
 		end
 		mp.observe_property("time-pos", "native", bouncing_loop)
+		local path = mp.get_property("path")
+		mp.msg.info(string.format("BOUNCE %s %s %s", loop_a, loop_b, path))
+		print(string.format("BOUNCE %s %s %s", loop_a, loop_b, path))
 		mp.osd_message("A-B bounce-loop " .. loop_a .. " - " .. loop_b)
 	else
 		mp.unobserve_property(bouncing_loop)
 		mp.osd_message("Clear A-B bounce loop")
-		mp.set_property("play-dir", "forward")
+		mp.set_property("play-direction", "forward")
 		loop_a = nil
 		loop_b = nil
 	end
